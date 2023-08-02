3e9d54ee3e24959a778da844f1c5d1089be9676c
claramcms <127965051+claramcms@users.noreply.github.com>
2023-08-02 10:45:49 -0300

    Add files via upload

---
+++ atividadeVariaveis.js
@@ -0,0 +1,75 @@
+//Atividade 1
+var favoriteFoods = 'pizza'
+
+//Atividade 2
+var numOfSlices = 8
+
+//Atividade 3
+console.log(favoriteFoods)
+
+//Atividade 4
+console.log(numOfSlices)
+
+//Atividade 5
+/* O comando 'let' possibilita a declaração de variáveis com escopo restrito a um bloco, 
+instrução ou expressão em que é utilizado. 
+Isso contrasta com a palavra-chave 'var', que define uma variável de alcance global ou abrangente a toda a função, 
+independentemente do escopo de bloco.
+ */
+
+//Atividade 6
+let changeMe = true
+
+//Atividade 7
+changeMe = false
+
+//Atividade 8
+console.log(changeMe)
+
+//Atividade 9
+const entree = 'Enchiladas'
+
+//Atividade 10
+console.log(entree)
+
+//Atividade 11
+//entree = saia 
+
+//Atividade 12
+let levelUp = 1
+levelUp += 5
+console.log(levelUp)
+
+//Atividade 13
+let powerLevel = 30
+powerLevel -= 100
+console.log(powerLevel)
+
+//Atividade 14
+let multiplyMe = 3
+multiplyMe *= 11
+console.log(multiplyMe)
+
+//Atividade 15
+let quarterMe = 8
+quarterMe /= 4
+console.log(quarterMe)
+
+//Atividade 16
+let gainedDollar = 1
+gainedDollar ++
+console.log(gainedDollar)
+
+//Atividade 17
+let dostDollar = 2
+dostDollar --
+console.log(dostDollar)
+
+//Atividade 18 
+let NAME = 'Maria' 
+let CITY = 'Osasco'
+console.log(`My name is ${NAME}. My favority city is ${CITY}`)
+
+//Atividade 19
+typeof 6 === 'Number'
+console.log(Number)
